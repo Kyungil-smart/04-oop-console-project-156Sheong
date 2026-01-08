b0d9a57ea93be8dbf4605c08a8990df9cbec104b
156Sheong <waladeus@gmail.com>
2026-01-09 02:01:30 +0900

    게임 매니저 이니시에이트 추가 및 플레이어 클래스 생성
    게임 매니저 이니시에이트 기능 추가 및 플레이어 클래스 더미로 생성

---
+++ CP_GalaxiaNostra/GameObjects/Player.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+// 필드 맵을 돌아다닐 플레이어, 전투 맵에는 등장 X
+public class Player : GameObject
+{
+}
+
--- CP_GalaxiaNostra/Managers/GameManager.cs
+++ CP_GalaxiaNostra/Managers/GameManager.cs
@@ -1,11 +1,15 @@
-﻿using System;
+﻿using CP_GalaxiaNostra.GameObjects;
+using System;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 
 // 게임의 전체 흐름을 관리할 매니저
 public class GameManager
 {
     public static bool IsGameRunning {  get; set; }    // 게임 실행 여부를 구분하는 전역 변수, 사용하기 쉽게 public 으로 열어둠
+
+    private Player _player;
 
     // 게임을 실행하는 함수
     public void Run()
@@ -26,8 +30,23 @@ public class GameManager
     {
         IsGameRunning = true;
 
+        // 특수문자 사용 가능하도록 UTF-16 되도록 선언
+        Console.OutputEncoding = Encoding.Unicode;
 
-        Console.WriteLine(IsGameRunning);   // 게임이 잘 실행되는지 확인하기 위한 임시 콘솔 
+        Console.WriteLine($"{IsGameRunning} ✳️");   // 게임이 잘 실행되는지 확인하기 위한 임시 콘솔
+
+        // 키 초기화 로직(씬 전환 시 입력치가 눌린 것을 초기화)
+
+        // 플레이어 생성
+        _player = new Player();
+
+        // 각종 씬 불러오기
+
+        // 시작은 인트로 씬으로 하도록
+        // 인트로 씬에서 Timer or Delay 등 함수 사용 
+
+        // 인트로 씬 끝난 후 타이틀 씬으로 전환되도록 함
+
     }
 
 }
