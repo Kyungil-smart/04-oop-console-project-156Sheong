e35ebc05385391bf82f2960a2aab89aef151a19b
156Sheong <waladeus@gmail.com>
2026-01-12 23:46:16 +0900

    전투용 UI 출력 텍스트 추가

---
+++ CP_GalaxiaNostra/GameDatas/UIDatas.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+public class UIDatas
+{
+    // vs 이미지를 출력하는 함수
+    public void PrintVSUI()
+    {
+        Console.WriteLine();
+        Console.ForegroundColor = ConsoleColor.Gray;
+        Console.Write("====================");
+        Console.ForegroundColor = ConsoleColor.DarkYellow;
+        Console.Write("   V  S   ");
+        Console.ForegroundColor = ConsoleColor.Gray;
+        Console.Write("====================");
+        Console.WriteLine();
+        Console.ResetColor();
+        Console.WriteLine();
+    }
+
+
+}
+
