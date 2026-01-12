086b38260231cc636b453111239b560577a01d07
156Sheong <waladeus@gmail.com>
2026-01-13 00:35:46 +0900

    함선 관련 스크립트 명칭 및 입력 수정

--- CP_GalaxiaNostra/GameDatas/PlayerDatas.cs
+++ CP_GalaxiaNostra/GameDatas/PlayerDatas.cs
@@ -40,7 +40,7 @@ public class PlayerEnemy001 : Player
 public class PlayerEnemy002 : Player
 {
     protected override TeamType TeamType { get; set; } = TeamType.EnemyTeam01;
-    protected override Warship[] pokemonOwned { get; set; } = new Warship[2];
+    protected override Warship[] pokemonOwned { get; set; } = new Warship[3];
 
     public PlayerEnemy002(int id, string name) : base(id, name)
     {
@@ -48,14 +48,14 @@ public class PlayerEnemy002 : Player
         // 생성 시 포켓몬 3마리 자동 생성
         pokemonOwned[0] = new Daring(3001, 1, TeamType.EnemyTeam01);
         pokemonOwned[1] = new Dragon(3002, 1, TeamType.EnemyTeam01);
-        //pokemonOwned[2] = new Tianjin(3003, 1, TeamType.EnemyTeam01);
+        pokemonOwned[2] = new Tianjin(3003, 1, TeamType.EnemyTeam01);
     }
 }
 
 public class PlayerEnemy003 : Player
 {
     protected override TeamType TeamType { get; set; } = TeamType.EnemyTeam01;
-    protected override Warship[] pokemonOwned { get; set; } = new Warship[2];
+    protected override Warship[] pokemonOwned { get; set; } = new Warship[3];
 
     public PlayerEnemy003(int id, string name) : base(id, name)
     {
@@ -63,7 +63,7 @@ public class PlayerEnemy003 : Player
         // 생성 시 포켓몬 6마리 자동 생성
         pokemonOwned[0] = new Corsair(3001, 1, TeamType.EnemyTeam01);
         pokemonOwned[1] = new Tokyo(3002, 1, TeamType.EnemyTeam01);
-        //pokemonOwned[2] = new Benelux(3003, 1, TeamType.EnemyTeam01);
+        pokemonOwned[2] = new Benelux(3003, 1, TeamType.EnemyTeam01);
     }
 }
 
