3ec8426efc42a83ad6edbd8fd7b638881471c2cc
156Sheong <waladeus@gmail.com>
2026-01-13 11:50:36 +0900

    승리 전리품 수정

--- CP_GalaxiaNostra/Managers/CombatManager.cs
+++ CP_GalaxiaNostra/Managers/CombatManager.cs
@@ -144,13 +144,26 @@ public class CombatManager
     // 승리 매서드
     public void Victory()
     {
-        // 전리품
-        GameManager._player.GetCoin(6);
-        GameManager._player.GetFuel(15);
+        Random randRewardNumA = new Random();
+        Random randRewardNumB = new Random();
+        Random randRewardNumC = new Random();
+        int reNumberA = randRewardNumA.Next(0, 5);
+        int reNumberB = randRewardNumB.Next(0, 4);
+        int reNumberC = randRewardNumC.Next(0, 12);
+
+        // 랜덤 전리품
+        if (reNumberC < 4) GameManager._player.GetFuel(4 + reNumberA);
+        else if (reNumberC < 8) GameManager._player.GetCoin(3 + reNumberB);
+        else
+        {
+            GameManager._player.GetFuel(4);
+            GameManager._player.GetCoin(3);
+        }
+
         // Player.battleCount++;
 
         SceneManager.ChangeScene("Field001");
-        
+
     }
 
     // 패배 매서드
